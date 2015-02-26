d222614abffe81752a520acd1dd949418bd1e65c
Blixthalka <emil837@gmail.com>
2015-02-26 23:10:31 +0100

    terrain collision

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -47,7 +47,7 @@ public class MainGameLoop {
 			entities.add(entity);
 		}
 
-		Light light = new Light(new Vector3f(100,20,10), new Vector3f(1, 1, 1));
+		Light light = new Light(new Vector3f(100,60,10), new Vector3f(1, 1, 1));
 
 		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grass"));
 		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("cash"));
@@ -57,9 +57,7 @@ public class MainGameLoop {
 		
 		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
 		
-		System.out.println("hej");
-		Terrain terrain = new Terrain(0, -1, loader,texturePack,blendMap);
-		Terrain terrain2 = new Terrain(0, -2, loader,texturePack,blendMap);
+		Terrain terrain = new Terrain(0, -1, loader,texturePack,blendMap, "heightmap");
 
 		
 		MasterRenderer renderer = new MasterRenderer();
@@ -69,11 +67,10 @@ public class MainGameLoop {
 		while (!Display.isCloseRequested()) {
 			// entity.increaseRotation(0.1f, 0.1f, 0);
 			camera.move();
-			player.move();
+			player.move(terrain);
 			
 			renderer.processEntity(player);
 			renderer.processTerrain(terrain);
-			renderer.processTerrain(terrain2);
 
 			for (Entity entity : entities) {
 				renderer.processEntity(entity);
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -6,6 +6,7 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
+import terrains.Terrain;
 
 public class Player extends Entity {
 
@@ -13,13 +14,13 @@ public class Player extends Entity {
 	private static final float TURN_SPEED = 160;
 	private static final float GRAVITY = -50;
 	private static final float JUMP_POWER = 30;
-	
+
 	private static final float TERRAIN_HEIGHT = 0;
 
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
 	private float upwardsSpeed = 0;
-	
+
 	private boolean isInAir = false;
 
 	public Player(TexturedModel model, Vector3f position, float rotX,
@@ -27,7 +28,7 @@ public class Player extends Entity {
 		super(model, position, rotX, rotY, rotZ, scale);
 	}
 
-	public void move() {
+	public void move(Terrain terrain) {
 		checkInputs();
 		super.increaseRotation(0,
 				currentTurnSpeed * DisplayManager.getFrameTimeSeconds(), 0);
@@ -38,21 +39,24 @@ public class Player extends Entity {
 				.cos(Math.toRadians(super.getRotY())));
 		super.increasePosition(dx, 0, dz);
 		upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
-		super.increasePosition(0, upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
-		if(super.getPosition().y < TERRAIN_HEIGHT) {
+		super.increasePosition(0,
+				upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
+		float terraingHeight = terrain.getHeightOfTerrain(
+				super.getPosition().x, super.getPosition().z);
+		if (super.getPosition().y < terraingHeight) {
 			upwardsSpeed = 0;
 			isInAir = false;
-			super.getPosition().y = TERRAIN_HEIGHT;
+			super.getPosition().y = terraingHeight;
 		}
 	}
 
 	private void jump() {
-		if(!isInAir){
-		this.upwardsSpeed = JUMP_POWER;
-		isInAir = true;
+		if (!isInAir) {
+			this.upwardsSpeed = JUMP_POWER;
+			isInAir = true;
 		}
 	}
-	
+
 	private void checkInputs() {
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
 			this.currentSpeed = RUN_SPEED;
@@ -69,7 +73,7 @@ public class Player extends Entity {
 		} else {
 			this.currentTurnSpeed = 0;
 		}
-		
+
 		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
 			jump();
 		}
